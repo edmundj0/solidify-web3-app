83d2028aec17effab3839bc10bd1d81a555aaaa9
Edmund Ju <edmundju@berkeley.edu>
2023-05-10 20:20:27 -0700

    modify package json to handle optional chaining

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import abi from "./utils/WavePortal.json"
+import { ethers } from "hardhat";
 
 const getEthereumObject = () => window.ethereum; //metamask injects this in browser
 
@@ -34,14 +36,57 @@ const findMetaMaskAccount = async () => {
 function App() {
 
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = '0xB9a9f4f998d0Aaceb0b18F6666edBB8E73598c42'
+  const contractABI = abi.abi
 
   useEffect(() => {
-    findMetaMaskAccount().then((account) => {
+    const getAccount = async () => {
+      const account = await findMetaMaskAccount();
       if (account !== null) {
-        setCurrentAccount(account)
+        setCurrentAccount(account);
       }
-    })
-  }, [])
+    }
+    getAccount()
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject()
+      if (!ethereum) {
+        alert("Get Metamask!")
+        return
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts"
+      })
+
+      console.log("wallet connected", accounts[0])
+      setCurrentAccount(accounts[0])
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+        let count = await wavePortalContract.getTotalWaves()
+        console.log("total wave count: ", count.toNumber())
+      } else {
+        console.log('ethereum object does not exist on window')
+      }
+    }catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className="main-container">
@@ -53,8 +98,10 @@ function App() {
           Connect your ethereum wallet and wave at me! {currentAccount}
         </div>
         <button className="waveButton" onClick={null}>Wave at me</button>
-
+        <button className="waveButton" onClick={connectWallet}>{currentAccount ? 'Wallet Connected!': 'Connect Wallet'}</button>
       </div>
+
+
     </div>
   );
 }
